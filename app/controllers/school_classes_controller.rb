18d9f9070c6ad50d1021e750261047836ebb6a26
Alex Lee <hyunho98@gmail.com>
2020-04-28 23:29:58 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -6,15 +6,21 @@ class SchoolClassesController < ApplicationController
 
   def create
     @schoolclass = SchoolClass.new(:title, :room_number)
+    @schoolclass.save
+    redirect_to schoolclass_path(@schoolclass)
   end
 
   def show
+    @schoolclass = SchoolClass.find(params[:id])
   end
 
   def edit
+    @schoolclass = SchoolClass.find(params[:id])
   end
 
   def update
+    @schoolclass = SchoolClass.find(params[:id])
+    @schoolclass.update(schoolclass_params(:title, :room_number))
   end
 
   private
