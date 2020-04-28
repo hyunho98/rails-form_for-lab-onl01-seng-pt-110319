bc82fa8e067a68280448669b628890f6065d6862
Alex Lee <hyunho98@gmail.com>
2020-04-28 23:28:06 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,26 @@
 class SchoolClassesController < ApplicationController
+
+  def new
+    @schoolclass = SchoolClass.new
+  end
+
+  def create
+    @schoolclass = SchoolClass.new(:title, :room_number)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def schoolclass_params(*args)
+    params.require(:schoolclass).permit(*args)
+  end
+  
 end
