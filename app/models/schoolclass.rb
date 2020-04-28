0b580c03b49f44302899e5d92e7bc724fbd9ce3f
Alex Lee <hyunho98@gmail.com>
2020-04-28 23:26:34 +0000

    Automatically backed up by Learn

--- app/models/schoolclass.rb
+++ app/models/schoolclass.rb
@@ -1,3 +1,23 @@
 class SchoolClass < ActiveRecord::Base
 
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def schoolclass_params(*args)
+    params.require(:schoolclass).permit(*args)
+  end
 end
